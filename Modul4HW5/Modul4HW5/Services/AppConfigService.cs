1021bb8018d5a665881b908e91c54f06a341d269
Yurii <leoonovn@gmail.com>
2021-09-08 12:41:56 +0300

    Сделал конфигурируемую строку подключения

---
+++ Modul4HW5/Modul4HW5/Services/AppConfigService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Modul4HW5.Configs;
+using Newtonsoft.Json;
+
+namespace Modul4HW5.Services
+{
+    public class AppConfigService
+    {
+        public AppConfigService()
+        {
+            var config = GetConfig();
+            ConnectionString = config.ConnectionString;
+        }
+
+        public string ConnectionString { get; }
+
+        private AppConfig GetConfig()
+        {
+            var json = File.ReadAllText("config.json");
+            var config = JsonConvert.DeserializeObject<AppConfig>(json);
+            return config;
+        }
+    }
+}
--- Modul4HW5/Modul4HW5/Starter.cs
+++ Modul4HW5/Modul4HW5/Starter.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modul4HW5.Services;
 
 namespace Modul4HW5
 {
     public class Starter
     {
+        private AppConfigService _config = new AppConfigService();
         public void Run()
         {
-            Console.WriteLine("develop");
+            Console.WriteLine(_config.ConnectionString);
         }
     }
 }
