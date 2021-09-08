997603a84bae4c69de7928ca1f520654bbc7920c
Yurii <leoonovn@gmail.com>
2021-09-08 12:28:19 +0300

    start master

---
+++ Modul4HW5/Modul4HW5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Modul4HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+            var starter = serviceProvider.GetService<Starter>();
+            starter.Run();
+        }
+    }
+}
